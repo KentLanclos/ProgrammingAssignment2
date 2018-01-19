14047dbacce077b40ae8d2acc2012b25d6dda4c6
KentLanclos <35151480+KentLanclos@users.noreply.github.com>
2018-01-19 12:22:01 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
-## R Programming Assignment 2: Lexical Scoping--caching the inverse of a matrix
-## The makeCacheMatrix function creates a special "matrix",
-## which is really a list containing a function to
-## 1. set the value of the matrix
-## 2. get the value of the matrix
-## 3. set the value of the inverse of the matrix
-## 4. get the value of the inverse of the matrix
+##  R Programming Assignment 2: Lexical Scoping--caching the inverse of a matrix
+##  The makeCacheMatrix function creates a special "matrix",
+##  which is really a list containing a function to
+##  1. set the value of the matrix
+##  2. get the value of the matrix
+##  3. set the value of the inverse of the matrix
+##  4. get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 z <- NULL
@@ -22,12 +22,12 @@ z <- NULL
         getinverse = getinverse)
  }
 
-## The cacheSolve function calculates the inverse of the special "matrix"
-## the special "matrix" which created with the makeCacheMatrix function.
-## However, it first checks to see if the inverse has already been calculated.
-## If so, it gets the inverse from the cache and skips the computation.
-## Otherwise, it calculates the inverse of the matrix and sets the value of the inverse
-## in the cache via the setinverse function.
+##  The cacheSolve function calculates the inverse of the special "matrix"
+##  the special "matrix" which created with the makeCacheMatrix function.
+##  However, it first checks to see if the inverse has already been calculated.
+##  If so, it gets the inverse from the cache and skips the computation.
+##  Otherwise, it calculates the inverse of the matrix and sets the value of the inverse
+##  in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
     # Return a matrix that is the inverse of 'x'    
